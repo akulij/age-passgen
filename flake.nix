f9210779642bddf6e6c5b1bfa00c34503babf875
akulij <aakulij@gmail.com>
2025-02-28 00:15:27 +0000

    add option to build and run this repo using nix

--- flake.nix
+++ flake.nix
@@ -8,6 +8,20 @@
                 let pkgs = nixpkgs.legacyPackages.${system}; in
                 {
                     devShells.default = import ./shell.nix { inherit pkgs; };
+
+                    packages.default = pkgs.buildGoModule {
+                        pname = "age-passgen";
+                        version = "unversioned";
+
+                        src = ./.;
+
+                        vendorHash = "sha256-Y6R8c9PzRq0tJ0b06f0LuFfrdFvxQ7h/86a6gg6UOro=";
+                    };
+
+                    apps.default = {
+                        type = "app";
+                        program = "${self.packages.${system}.default}/bin/age-passgen";
+                    };
                 }
             );
 }
